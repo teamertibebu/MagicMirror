e9833315dc07c5a67d6e2d3d5422a8c533e72f1c
Teamer Tibebu <teamertibebu@gmail.com>
2025-05-27 00:08:14 -0400

    first commit - project setup

---
+++ magic_mirror.ex
@@ -0,0 +1,18 @@
+defmodule MagicMirror do
+  @moduledoc """
+  Documentation for `MagicMirror`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MagicMirror.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
